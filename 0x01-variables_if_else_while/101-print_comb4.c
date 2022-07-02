b153c2859fa605cf6358b48b7e96190e1ebb4e50
Jockthan <jasminblinkz@gmail.com>
2022-07-01 21:04:51 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,9 +10,11 @@ int main(void)
 	for (var = 0; var < 8; var++)
 	{
 		int ii;
+
 		for (ii = var + 1; ii < 9; ii++)
 		{
 			int jj;
+
 			for (jj = ii + 1; jj < 10; jj++)
 			{
 			putchar((var % 10) + '0');
